51d30e8746cee851048114961c74be0892bcc024
utsho-anik <anikdas548@gmail.com>
2023-03-28 16:52:26 +0600

    change in  master

---
+++ app/src/main/java/com/techsoldev/tictactoegame/Demo.java
@@ -0,0 +1,46 @@
+package com.techsoldev.tictactoegame;
+
+public class Demo {
+
+    int a,b;
+    String name , address;
+
+    public Demo(int a, int b, String name, String address) {
+        this.a = a;
+        this.b = b;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
